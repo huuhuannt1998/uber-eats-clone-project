710e5e29947e46640af0dc2532dba4d1f758852e
Huan Bui <huuuhuannt1998@gmail.com>
2022-01-08 01:13:13 -0500

    yelp api

--- screens/Home.js
+++ screens/Home.js
@@ -3,10 +3,17 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { ScrollView } from 'react-native-web';
 import Categories from '../components/Categories';
 import HeaderTabs from '../components/HeaderTabs';
-import RestaurantItems from '../components/RestaurantItems';
+import RestaurantItems, {
+    localRestaurants,
+  } from '../components/RestaurantItems';
 import SearchBar from '../components/SearchBar';
 
+
+
 export default function Home() {
+
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
     return (
         <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
             <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -15,7 +22,9 @@ export default function Home() {
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
-                <RestaurantItems />
+                <RestaurantItems 
+                    restaurantData={restaurantData}
+                />
             </ScrollView>
         </SafeAreaView>
     );
